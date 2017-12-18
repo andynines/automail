021755e564d71813fdf9750e6f70eaef33f7c617
andynines <andrewsenin@comcast.net>
2017-12-18 12:06:29 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,21 +6,27 @@ MIT License
 
 import os
 
+#connection settings
 USERNAME = "myemail@provider.com" #email address from which the messages are sent
 PASSWORD = "mypassword" #the email account's respective password
 HOST = "smtp.provider.com" #smtp server domain name
-PORT = 587 #port number to connect to
+PORT = 555  #port number to connect to
 SSL = False #connect to SMTP server with SSL encryption
 TLS = True #use TLS encryption when sending emails
 
-RECIPIENTS = os.path.join("records", "recipients.txt") #path to list of recipients' addresses
-SUBJECT = "Your Daily Quote has Arrived" #string to appear in subject of each email
-CONTENT = os.path.join("records", "content.txt") #path to content file to pull material from
-MESSAGE = os.path.join("records", "message.txt") #path to file where possible server messages are stored
+#message settings
+RECIPIENTS = ["buddy@provider.com", #recipients' email addresses
+              "anotherrecipient@anotherprovider.com",
+              "lastexample@lastprovider.com"]
+SUBJECT = "Your daily quote has arrived" #string to appear in subject of each email
+CONTENT = os.path.join("records", "quotes.txt") #path to content file to pull material from
 END_MESSAGE = "Have a nice day!" #message that appears at the end of emails
+URL = "https://www.github.com/andynines/automail" #link to the github repo
 
+#log settings
 LOG = os.path.join("records", "log.txt") #path to file which logger will write and manage
-MAX_LINES = 200 #maximum number of lines before the logger begins to delete old entries
+MAX_LINES = 300 #maximum number of lines before the logger begins to delete old entries
 
-SEND_INTERVAL = 24 #how many hours between routines
-SLEEP_TIME = 5 #how many seconds between time checks during standby
+#timer settings
+SEND_HOUR = 7 #hour at which emails are sent daily; use 24-hour clock
+SLEEP_TIME = 60 #second interval between time checks
