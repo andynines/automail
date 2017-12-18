2a94da7e18f7800c413b5f30005eb0d469cd8457
andynines <andrewsenin@comcast.net>
2017-12-18 12:03:43 -0500

    Update launch.py

--- launch.py
+++ launch.py
@@ -10,24 +10,19 @@ import datetime, time
 def main():
     """
     Initializes the environment for a new automail bot, and schedules it to run
-    as specified in the settings module.
+    when specified in the settings module.
     """
+    ready = True
     logger.initialize()
-    logger.log("New automail bot initialized")
-    send_interval = settings.SEND_INTERVAL
-    sleep_time = settings.SLEEP_TIME
+    logger.log("Automail launched\nPress Ctrl+C to terminate bot")
     while True:
-        bot.routine()
-        current_dt = datetime.datetime.now()
-        logger.log("Going on standby at {}.{}.{}.{}.{}.{}".format(current_dt.year,
-                                                                  current_dt.month,
-                                                                  current_dt.day,
-                                                                  current_dt.hour,
-                                                                  current_dt.minute,
-                                                                  current_dt.second))
-        next_dt = current_dt + datetime.timedelta(hours=send_interval)
-        while datetime.datetime.now() < next_dt:
-            time.sleep(sleep_time)
+        current_hour = datetime.datetime.now().hour
+        if current_hour == settings.SEND_HOUR and ready:
+            bot.routine()
+            ready = False
+        elif current_hour == (settings.SEND_HOUR + 1) % 24:
+            ready = True
+        time.sleep(settings.SLEEP_TIME)
 
 if __name__ == "__main__":
     main()
