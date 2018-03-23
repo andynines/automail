fb3ec5de7554f5d0a234203c5500eaebe599d366
andynines <andrewsenin@comcast.net>
2018-03-23 16:52:44 -0400

    New method of preventing repeat selections

--- compose.py
+++ compose.py
@@ -5,9 +5,22 @@ MIT License
 """
 
 import logger, settings
-import random
+import math, random
 
-prev = None #store previous quote to prevent repeats
+def get_previous(read_bytes):
+    """
+    Retrieve the index of the last selection to appear in an email.
+    """
+    with open(settings.PREVIOUS, "r") as file:
+        index = int(file.read(read_bytes))
+    return index
+    
+def set_previous(index):
+    """
+    Set an index to not be a possible selection for the next piece of content.
+    """
+    with open(settings.PREVIOUS, "w") as file:
+        file.write(str(index))
 
 def daily_selection():
     """
@@ -15,13 +28,16 @@ def daily_selection():
     by a newline; every line is a new piece of content.
     """
     logger.log("Selecting today's material")
+    
     with open(settings.CONTENT, "r") as file:
-        lines = file.readlines()
-    selection = (random.choice(lines[0:prev] + lines[prev + 1:])
-                 if prev else
-                 random.choice(lines))
+        content = file.readlines()
+    lines = len(content)
+    prev = get_previous(int(math.log10(lines)))
+    selection_index = random.choice(list(range(prev)) + list(range(prev + 1, lines)))
+    selection = content[selection_index]
     selection += ("\n" if selection[-1] != "\n" else "")
     logger.log("Selected: " + selection, newline=False)
+    set_previous(selection_index)
     return selection
 
 def message():
